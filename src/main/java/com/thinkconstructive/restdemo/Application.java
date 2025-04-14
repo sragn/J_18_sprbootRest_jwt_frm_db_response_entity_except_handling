0b1fdd52028af6d80a6a9fa6ff47c0b61efef511
Admin <Admin@DESKTOP-KSOO4CR>
2025-04-14 21:57:52 +0530

    spring to support apache tomcat

--- src/main/java/com/thinkconstructive/restdemo/Application.java
+++ src/main/java/com/thinkconstructive/restdemo/Application.java
@@ -12,6 +12,8 @@ public class Application extends SpringBootServletInitializer {
 	    return builder.sources(Application.class);
 	  }
 	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
